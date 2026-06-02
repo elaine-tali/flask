48de56adaa3c3256fa19ce515e7051a7afe726b8
elaine-tali <elainetalitaferreira@gmail.com>
2026-06-02 20:37:09 -0300

    atualização rota

--- estudo/views.py
+++ estudo/views.py
@@ -1,7 +1,12 @@
 from estudo import app
-from flask import render_template
+from flask import render_template, url_for
 
 @app.route('/') #é a rota apos a barra no navegador
 
 def homepage(): #é uma função para retornar 
     return render_template('index.html')
+
+@app.route('/nova/') 
+
+def novapage():  
+    return 'outra view'
